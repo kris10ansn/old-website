e98abdef3fbcc880df8cc492883bdb5974bd9955
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2020-03-19 19:18:47 +0100

    remove console.log

--- js/main.js
+++ js/main.js
@@ -1,62 +1,64 @@
 function smoothScroll() {
-	function distanceToTop(element) {
-		return Math.floor(element.getBoundingClientRect().top);
-	}
+    function distanceToTop(element) {
+        return Math.floor(element.getBoundingClientRect().top);
+    }
 
-	function anchorLinkHandler(event) {
-		event.preventDefault();
-		const targetID = this.getAttribute("href");
-		const targetAnchor = document.querySelector(targetID);
-		if (!targetAnchor) return;
+    function anchorLinkHandler(event) {
+        event.preventDefault();
+        const targetID = this.getAttribute("href");
+        const targetAnchor = document.querySelector(targetID);
+        if (!targetAnchor) return;
 
-		const originalTop = distanceToTop(targetAnchor);
+        const originalTop = distanceToTop(targetAnchor);
 
-		window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
-		location.hash = "";
-	}
+        window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+        location.hash = "";
+    }
 
-	const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+    const linksToAnchors = document.querySelectorAll('a[href^="#"]');
 
-	linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
+    linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
 }
 
 function loadImage() {
-	const img = document.querySelector("img.jumbo");
-	const startTime = Date.now();
-	const anim = "--img-anim-delay";
+    const img = document.querySelector("img.jumbo");
+    const startTime = Date.now();
+    const anim = "--img-anim-delay";
 
-	img.onload = () => {
-		const styles = window.getComputedStyle(img);
-		const animDelay = (parseFloat(styles.getPropertyValue(anim)) || 0) * 1000;
-		const loadTime = Date.now() - startTime;
+    img.onload = () => {
+        const styles = window.getComputedStyle(img);
+        const animDelay =
+            (parseFloat(styles.getPropertyValue(anim)) || 0) * 1000;
+        const loadTime = Date.now() - startTime;
 
-		const newAnimDelay = Math.max(0, animDelay - loadTime);
-		img.style.setProperty(anim, `${newAnimDelay / 1000}s`);
+        const newAnimDelay = Math.max(0, animDelay - loadTime);
+        img.style.setProperty(anim, `${newAnimDelay / 1000}s`);
 
-		img.style.display = "block";
-	};
+        img.style.display = "block";
+    };
 
-	img.src = "assets/code.png";
+    img.src = "assets/code.png";
 }
 
 function removeImageDrag() {
-	const dont = () => false;
-	document
-		.querySelectorAll("img, svg")
-		.forEach(img => (img.ondragstart = dont));
+    const dont = () => false;
+    document
+        .querySelectorAll("img, svg")
+        .forEach(img => (img.ondragstart = dont));
 }
 
 function mobileNav() {
-	const hamburgerButton = document.querySelector("#hamburger-button");
-	const mobileNav = document.querySelector("mobile-nav");
+    const hamburgerButton = document.querySelector("#hamburger-button");
+    const mobileNav = document.querySelector("mobile-nav");
 
-	hamburgerButton.addEventListener("click", () => {
-		console.log("open");
-		mobileNav.classList.add("open");
-		mobileNav.classList.add("opened");
-	});
+    hamburgerButton.addEventListener("click", () => {
+        mobileNav.classList.add("open");
+        mobileNav.classList.add("opened");
+    });
 
-	mobileNav.addEventListener("click", () => mobileNav.classList.remove("open"));
+    mobileNav.addEventListener("click", () =>
+        mobileNav.classList.remove("open")
+    );
 }
 
 loadImage();
